e0b29833f0ebe4a322ebad1a74619ac0e67e26f2
SsTommyChensS <tranquoctunglun@gmail.com>
2023-07-26 17:51:30 +0700

    Refactor: errorHandler -> return response depend on each status code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,36 @@
-const enviroment_variables = require('../configs/envVariablesConfig');
+const enviromentVariables = require('../configs/envVariablesConfig');
 
 const ErrorHandler = (err, req, res, next) => {
     const errStatus = err.status || 500;
-    const errMsg = err.message || 'Something went wrong';
-    res.status(errStatus).send({
-        status: false,
-        statusCode: errStatus,
-        message: errMsg,
-    })
+    const errMsg = err.message_errors || err.message || 'Something went wrong';
+
+    switch(errStatus) {
+        case 401: case 404: case 409: {
+            res.status(errStatus).send({
+                status: false,
+                statusCode: errStatus,
+                message: errMsg,
+            });
+            break;
+        }
+        case 422: {
+            res.status(errStatus).send({
+                status: false,
+                statusCode: errStatus,
+                errors: errMsg,
+            });
+            break;
+        }
+        case 500: {
+            res.status(errStatus).send({
+                status: false,
+                statusCode: errStatus,
+                message: errMsg,
+                stack: enviromentVariables.server_enviroment === 'development' ? err.stack : {}
+            });
+            break;
+        }
+    }
 }
 
 module.exports = ErrorHandler;
